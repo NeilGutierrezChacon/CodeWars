22c43fbedb61de0278481b9daafc3864b3fbf53a
NeilGutierrezChacon <a18neigutcha@inspedralbes.cat>
2020-07-04 01:54:43 +0200

    Mi primer ejercicio

---
+++  ArrayDiff/index.js
@@ -0,0 +1,14 @@
+function arrayDiff(a, b) {
+  b.forEach((element) => {
+    a = a.filter((value, index, array) => {
+      return value != element;
+    });
+  });
+  return a;
+}
+
+console.log(arrayDiff([], [4,5]));
+
+console.log(arrayDiff([3,4], [3]));
+
+console.log(arrayDiff([1,8,2], []));

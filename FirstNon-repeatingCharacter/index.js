15ad34f95d010a7eacd5ea106444ad0417a6fb9a
NeilGutierrezChacon <a18neigutcha@inspedralbes.cat>
2020-07-04 22:44:51 +0200

    Segundo ejercicio de codewawrs

---
+++ FirstNon-repeatingCharacter/index.js
@@ -0,0 +1,16 @@
+function firstNonRepeatingLetter(s) {
+  for (i = 0; i < s.length; i++) {
+    let cont = 0;
+    for (j = 0; j < s.length; j++) {
+      if (s[i].toLowerCase() == s[j].toLowerCase()) cont++;
+    }
+    if (cont <= 1) {
+      return s[i];
+    }
+  }
+  return "";
+}
+
+console.log(firstNonRepeatingLetter("a"));
+console.log(firstNonRepeatingLetter("stress"));
+console.log(firstNonRepeatingLetter("moonmen"));
